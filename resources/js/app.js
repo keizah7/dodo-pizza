51f6198a68dbcbaa6eb380dc63fc11b5c7c852ac
keizah7 <murashka.arturas@gmail.com>
2020-01-15 14:25:59 +0200

    Carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ function toggleMobileNav() {
 var navbar = document.querySelector("nav");
 var sticky = navbar.offsetTop;
 
+
 function hideHeader() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("nav--sticky")
@@ -19,3 +20,35 @@ function hideHeader() {
 window.onscroll = function () {
     this.hideHeader()
 };
+
+/* Carousel */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlides(count) {
+    showSlides(slideIndex += count);
+}
+
+function switchToSlide(number) {
+    showSlides(slideIndex = number);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName('carousel__slide');
+    let dots = document.getElementsByClassName('carousel__dot');
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace('active', '');
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+}
