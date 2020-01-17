39b75c21a1a2a6f90bcce4f8240904613d120f41
keizah7 <murashka.arturas@gmail.com>
2020-01-17 18:49:59 +0200

    Types seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TypesTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
