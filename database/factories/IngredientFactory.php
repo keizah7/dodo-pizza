99a97fc7f00fb707a1c4efa3f631af3a2b3c7f8c
keizah7 <murashka.arturas@gmail.com>
2020-01-18 17:12:01 +0200

    Ingredients crud #22

---
+++ database/factories/IngredientFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Ingredient;
+use Faker\Generator as Faker;
+
+$factory->define(Ingredient::class, function (Faker $faker) {
+    return [
+        'title' => $faker->word,
+        'removable' => $faker->boolean,
+    ];
+});
