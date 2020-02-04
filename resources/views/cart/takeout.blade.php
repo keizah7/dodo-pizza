6fe58168929e9f99ced25b4b300599febab12433
keizah7 <murashka.arturas@gmail.com>
2020-02-04 16:30:10 +0200

    Takeout

--- resources/views/cart/takeout.blade.php
+++ resources/views/cart/takeout.blade.php
@@ -4,42 +4,60 @@
 <section class="cart">
     <h2 class="cart__title">Išsinešimui</h2>
     <div class="cart__content">
-        <form action="{{ route('cart.store') }}" method="post">
+        <form action="{{ route('cart.store') }}" method="post" class="form">
             @csrf
-            <div>
-                <label>
-                    Vardas
-                    <input type="text" name="name" required>
-                    @error('name')<p class="help is-danger">{{ $message }}</p>@enderror
-                </label>
-            </div>
-            <div>
-                <label>
-                    El. paštas
-                    <input type="email" name="email" required>
-                    @error('email')<p class="help is-danger">{{ $message }}</p>@enderror
-                </label>
-            </div>
-            <div>
-                <label for="">Komentaras</label><textarea name="comment" id="" cols="30" rows="10"></textarea>
-                @error('comment')<p class="help is-danger">{{ $message }}</p>@enderror
-            </div>
 
-            <div>
-                <label for="">Išsinešimui</label>
-                @foreach($pickups as $pickup)
-                    <div>
-                        <input type="radio" name="pickup_id" value="{{ $pickup->id }}"  required> <b>{{ $pickup->title }}</b> {{ $pickup->address }}
-                        <p>
-                            {{ $pickup->description }}
-                        </p>
-                    </div><br>
-                @endforeach
-                @error('pickup_id')<p class="help is-danger">{{ $message }}</p>@enderror
+            <div class="field">
+                <label for="">@lang('data.name')</label>
+                <div class="control">
+                    <input name="name" type="text" placeholder="@lang('data.name.placeholder')" value="{{ old('name') }}" class="input" required>
+                </div>
             </div>
+            @error('name')<p class="help is-danger">{{ $message }}</p>@enderror
 
+            <div class="field">
+                <label for="">@lang('data.email')</label>
+                <div class="control">
+                    <input name="email" type="email" placeholder="@lang('data.email.placeholder')" value="{{ old('email') }}" class="input" required>
+                </div>
+            </div>
+            @error('email')<p class="help is-danger">{{ $message }}</p>@enderror
 
-            <button type="submit">Uzsakyti</button>
+            <div class="field">
+                <label for="">@lang('data.comment')</label>
+                <div class="control">
+                    <textarea name="comment" rows="5" placeholder="@lang('data.comment.placeholder')">{{ old('comment') }}</textarea>
+                </div>
+            </div>
+            @error('comment')<p class="help is-danger">{{ $message }}</p>@enderror
+
+            <div class="field">
+                <label for="">@lang('data.pickup_id')</label>
+                <div class="control">
+                    @foreach($pickups as $pickup)
+                        <div>
+                            <label for="">
+                            <input type="radio" name="pickup_id" value="{{ $pickup->id }}"  required>
+
+                                <b>{{ $pickup->title }}</b>
+                                <p>{{ $pickup->address }}</p>
+                                <p>{{ $pickup->description }}</p>
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            @error('pickup_id')<p class="help is-danger">{{ $message }}</p>@enderror
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <a href="{{ route('cart.index') }}"
+                        class="btn btn--white btn--square">@lang('data.button.back')</a>
+                </div>
+                <div class="control">
+                    <button type="submit" class="btn is-primary">@lang('data.button.order')</button>
+                </div>
+            </div>
         </form>
     </div>
 </section>
